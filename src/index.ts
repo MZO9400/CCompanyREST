d5667f6acd217c70e6412618bb3b3a85a14e151f
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 16:53:17 +0500

    Create API docs for getCompanies

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import * as dotenv from 'dotenv';
 import loginRouter from './routes/login';
 import registerRouter from './routes/register';
 import {options} from '../swaggerOptions';
+import getCompanies from "./routes/getCompanies";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.get('/docs', swaggerUI.setup(apiSpecs, { explorer: true }));
 
 app.use('/api/v1/login', loginRouter);
 app.use('/api/v1/login', registerRouter);
+app.use('/api/v1/getCompanies', getCompanies)
 
 if (!process.env.MONGOURI) {
   throw new Error('MONGOURI is not defined');
---
+++ src/routes/getCompanies.ts
@@ -0,0 +1,83 @@
+import express from 'express';
+import authHandler from "../middleware/authHandler";
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * paths:
+ *   /getCompanies:
+ *     get:
+ *       security:
+ *         - bearerAuth: []
+ *       responses:
+ *         '200':
+ *           description: OK
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/CompaniesResponse'
+ *         '401':
+ *           description: Unauthorized
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/GeneralResponse'
+ *
+ * components:
+ *   securitySchemes:
+ *     bearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ *   schemas:
+ *     CompaniesResponse:
+ *       type: object
+ *       properties:
+ *         status:
+ *           type: boolean
+ *           example: true
+ *         message:
+ *           type: string
+ *           example: OK
+ *         data:
+ *           type: array
+ *           items:
+ *             $ref: '#/components/schemas/Company'
+ *     Company:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *         name:
+ *           type: string
+ *           example: C Company
+ *         address:
+ *           type: string
+ *           example: Street 1, Boulevard 2, Avenue 3
+ *         phone:
+ *           type: string
+ *           example: +1 (123) 456-7890
+ *         geolocation:
+ *           type: object
+ *           properties:
+ *             latitude:
+ *               type: number
+ *               example: 37.4224764
+ *             longitude:
+ *               type: number
+ *               example: -122.0842499
+ *         logo:
+ *           type: string
+ *           example: https://www.example.com/logo.png
+ *         description:
+ *           type: string
+ *           example: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ *
+ * */
+router.get('/', authHandler, (req, res) => {
+
+});
+
+
+export default router;
