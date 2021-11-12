787e1fe27855f1c8acdc22c7c3c3babf50aa73fe
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 13:27:52 +0500

    Create Mongoose User model based on User interface

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,25 @@
-export default interface User {
+import mongoose from "mongoose";
+
+export interface User extends mongoose.Document {
     email: string,
     password: string,
     name: string,
 }
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+export const UserModel = mongoose.model<User>("User", userSchema);
