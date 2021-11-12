15441bc7b0ea80d4bd9976095fb1564f3cb03fdb
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 13:29:29 +0500

    Create Mongoose Company model based on User interface

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -1,4 +1,7 @@
-export default interface Company {
+import * as mongoose from "mongoose";
+
+
+export interface Company extends mongoose.Document {
     name: string;
     phoneNumber: string;
     address: string;
@@ -6,6 +9,46 @@ export default interface Company {
         lat: number;
         lng: number;
     };
-    logo: string;
+    logo?: string;
     description: string;
 }
+
+const companySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        geolocation: {
+            lat: {
+                type: Number,
+                required: true,
+            },
+            lng: {
+                type: Number,
+                required: true,
+            },
+        },
+        logo: {
+            type: String,
+            required: false,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const CompanyModel = mongoose.model<Company>("Company", companySchema);
