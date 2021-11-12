3763943420b07fd38c22788fbd3755a467973f40
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 12:18:16 +0500

    Install swagger API doc generatorand types

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,24 @@ import express from 'express';
 import mongoose from 'mongoose'
 import { json } from 'body-parser';
 import { todoRouter } from './routes/todo'
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
 const app = express()
 app.use(json())
 app.use(todoRouter)
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title:'C Company server API',
+      version:'1.0.0'
+    }
+  },
+  apis:['index.ts'],
+}
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
 mongoose.connect('mongodb://localhost:27017/test-todo', {
   useNewUrlParser: true,
