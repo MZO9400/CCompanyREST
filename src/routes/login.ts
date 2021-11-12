28cf4f2d659ec8d2d3e23b76ef246acc065979f7
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 16:29:39 +0500

    Login API: Refactor to send json

--- src/routes/login.ts
+++ src/routes/login.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {User, UserModel} from "../models/User";
+import {IUser, UserModel} from "../models/User";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
@@ -11,7 +11,7 @@ interface LoginResponse {
     name: string
   }
   token: string;
-  status: true,
+  status: boolean,
   message: string;
 }
 
@@ -23,21 +23,53 @@ interface LoginResponse {
  *       responses:
  *         '200':
  *           description: OK
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/LoginResponse'
  *         '500':
  *           description: Internal Server Error
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/GeneralResponse'
  *         '401':
  *           description: Password is invalid
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/GeneralResponse'
  *         '404':
  *           description: User Not Found
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/GeneralResponse'
  *     parameters:
  *       - in: body
  *         description: User object
  *         schema:
- *           $ref: '#/components/schemas/Login'
+ *           $ref: '#/components/schemas/LoginRequest'
  *
  * components:
  *   schemas:
- *     Login:
+ *     LoginResponse:
+ *       type: object
+ *       properties:
+ *         user:
+ *           type: object
+ *           properties:
+ *             email:
+ *               type: string
+ *             name:
+ *               type: string
+ *         token:
+ *           type: string
+ *         status:
+ *           type: boolean
+ *         message:
+ *           type: string
+ *     LoginRequest:
  *       type: object
  *       properties:
  *         email:
@@ -48,15 +80,15 @@ interface LoginResponse {
 router.post('/', (req, res) => {
   const { password, email } = req.body;
 
-  UserModel.findOne({ email }, (err: Error, user: User) => {
+  UserModel.findOne({ email }, (err: Error, user: IUser) => {
     if (err) {
-      res.status(500).send(err);
+      res.status(500).json({status: false, message: "Internal Server Error"});
     } else if (!user) {
-      res.status(404).send('User not found');
+      res.status(404).json({status: false, message: 'User not found'});
     } else {
       bcrypt.compare(password, user.password, (err, isMatch) => {
         if (err) {
-          res.status(500).send(err);
+          res.status(500).json({status: false, message: "Internal Server Error"});
         } else if (isMatch) {
           if (!process.env.SALT) {
             throw new Error('SALT is not defined');
@@ -75,13 +107,13 @@ router.post('/', (req, res) => {
               email: user.email,
               name: user.name
             },
-            token: token,
+            token,
             status: true,
             message: 'Login Successful'
           };
-          res.status(200).send(loginResponse);
+          res.status(200).json(loginResponse);
         } else {
-          res.status(401).send('Password is incorrect');
+          res.status(401).json({status: false, message: 'Password is incorrect'});
         }
       });
     }
