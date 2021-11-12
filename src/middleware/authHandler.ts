ea1a57c22470345b6987356e18fed0b8a40de495
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 20:55:07 +0500

    Middleware: Follow the same req/res architecture

--- src/middleware/authHandler.ts
+++ src/middleware/authHandler.ts
@@ -5,7 +5,7 @@ export default (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization']
     const token = authHeader?.split(' ')[1]
 
-    if (token == null) return res.sendStatus(401)
+    if (token == null) return res.status(401).json({status: false, message: 'No token provided'})
 
     jwt.verify(token, process.env.SALT as string, (err: Error) => {
         if (err) return res.status(401).json({status: false, message: 'Token is not valid'})
