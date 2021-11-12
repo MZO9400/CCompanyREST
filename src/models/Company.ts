c685c602a1863497661a54a201e45ca08f8ea48f
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 20:51:12 +0500

    Companies data

--- src/models/Company.ts
+++ src/models/Company.ts
@@ -1,7 +1,9 @@
 import * as mongoose from "mongoose";
+import {MongoClient} from "mongodb";
+import {generateRandomPhoneNumber} from "../helpers/Company";
+import fetchRandomImage from "../helpers/Images";
 
-
-export interface Company extends mongoose.Document {
+export interface ICompany {
     name: string;
     phoneNumber: string;
     address: string;
@@ -11,7 +13,10 @@ export interface Company extends mongoose.Document {
     };
     logo?: string;
     description: string;
+
 }
+
+export interface ICompanyMongoose extends mongoose.Document, ICompany {}
 
 const companySchema = new mongoose.Schema(
     {
@@ -51,4 +56,44 @@ const companySchema = new mongoose.Schema(
     }
 );
 
-export const CompanyModel = mongoose.model<Company>("Company", companySchema);
+export const getAllCompanies = (URL: string): Promise<any> => {
+    return new Promise((res, rej) => {
+        MongoClient.connect(URL, async (err, db) => {
+            if (err) {
+                rej(err)
+            } else {
+                const dbo = db?.db("mad")?.collection('ccompany');
+                const results = await dbo?.find({});
+                results?.toArray((err, result) => {
+                    if (err) {
+                        rej(err)
+                    } else {
+                        res(result)
+                    }
+                });
+            }
+        })
+    })
+}
+
+export const toICompany = async (data: any[]): Promise<ICompany[]> => {
+    return new Promise((res, rej) => {
+        const promises = data.map(async (company: any) => ({
+            id: company._id,
+            name: company.name,
+            phoneNumber: generateRandomPhoneNumber(),
+            address: company.vicinity,
+            geolocation: {
+                lat: company.geometry.location.lat,
+                lng: company.geometry.location.lng,
+            },
+            logo: await fetchRandomImage(),
+            description: company.types.map(
+                (i: string) => i[0].toUpperCase() + i.slice(1).replace("_", " ")
+            ).join(", "),
+        }))
+        Promise.all(promises).then(res).catch(rej)
+    })
+}
+
+export const CompanyModel = mongoose.model<ICompanyMongoose>("Company", companySchema);
