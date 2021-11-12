ff0a272f0be37afb3812d35771d1b0095a405dc4
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-12 13:23:17 +0500

    Create Interfaces

---
+++ src/models/Company.ts
@@ -0,0 +1,11 @@
+export interface Company {
+    name: string;
+    phoneNumber: string;
+    address: string;
+    geolocation: {
+        lat: number;
+        lng: number;
+    };
+    logo: string;
+    description: string;
+}
