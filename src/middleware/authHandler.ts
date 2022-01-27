c69a801a0c4716a63e0ec4d4bdff2dc204b748c7
MZO9400 <hamza.hameed00@gmail.com>
2022-01-27 05:54:23 +0500

    Auth middleware: Only pass authenticated variable

--- src/middleware/authHandler.ts
+++ src/middleware/authHandler.ts
@@ -5,10 +5,11 @@ export default (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization']
     const token = authHeader?.split(' ')[1]
 
-    if (token == null) return res.status(401).json({status: false, message: 'No token provided'})
-
     jwt.verify(token, process.env.SALT as string, (err: Error) => {
-        if (err) return res.status(401).json({status: false, message: 'Token is not valid'})
+        if (!err) {
+            req.body.authorized = true
+        }
         next()
     })
+
 }
